b00cc9b0661b0016cb5b8ef75cc7d36e052069c0
djehuty94 <romankastl@gmail.com>
2017-09-28 23:18:57 +0200

    Page test #1
    Trying to add a page

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { StackNavigator } from 'react-navigation';
+
+
+class HomeScreen extends Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    return <Text>Hello, Navigation!</Text>;
+  }
+}
+
+
+class Screen2 extends Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    return <Text>Hello, Navigation!</Text>;
+  }
+}
+
+//ADD Button to switch screen
 
 export default class App extends React.Component {
   render() {
+    const MainNavigator = StackNavigator({
+      Home:{screen: HomeScreen},
+      screen_1:{screen:Screen2},
+    })
+
+  
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={this.store}>
+        <View style={styles.container}>
+          <MainNavigator />
+        </View>
+      </Provider>
     );
   }
 }
