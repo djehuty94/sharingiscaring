44d3a2405a81d975301f2e57f590bbaedf5ad188
djehuty94 <romankastl@gmail.com>
2017-10-27 10:26:16 +0200

    Auth change

---
+++ js/auth_OLD.js
@@ -0,0 +1,43 @@
+//DOCUMENTATION
+// https://firebase.google.com/docs/auth/web/manage-users
+
+import firebase from 'firebase'; // Import Firebase login
+import { firebaseConfig } from './config/firebase_config.js'; // Import of Firebase config
+
+//console.log(firebaseConfig);
+//firebase.initializeApp(firebaseConfig);
+
+
+//Checked if user is logged
+
+//Return the corresponding boolean true/false
+
+
+//Check if user is logged in and return the session variable user
+export const isSignedIn = () =>{
+    return new Promise((resolve, reject) => {
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+              // User is signed in.
+              console.log("checked logged true");
+              console.log(user);
+              resolve(true);
+            } else {
+              // No user is signed in.
+              console.log("checked logged false");
+              console.log(false);
+              resolve(false); // TO BE CHANGED to true to bypass login
+            }
+        });
+    });
+};
+
+export const onSignOut =() => {
+    firebase.auth().signOut().then(function() {
+    console.log("Sign Out successfull");
+    // Sign-out successful.
+  }, function(error) {
+    console.log(error);
+    // An error happened.
+  });
+}
