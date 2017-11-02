0b639bd9f6aaf989326214f41599c499a63bdc96
tom-eberle <eberle.tom@gmail.com>
2017-11-02 08:01:16 +0100

    Publish redirection

--- js/components/pages/publish.js
+++ js/components/pages/publish.js
@@ -172,7 +172,8 @@ class Publish extends Component {
             price,
             uid,
            
-          });
+          })
+        .then(() => this.props.navigation.navigate("OfferDisplay"/*, {section: data.section}*/));
     
         }
         catch (error) {
