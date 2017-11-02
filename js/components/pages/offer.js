cbdf1b166e36d9b5bbf0b440c60fe588949660eb
tom-eberle <eberle.tom@gmail.com>
2017-11-02 18:54:05 +0100

    Display announce

--- js/components/pages/offer.js
+++ js/components/pages/offer.js
@@ -36,12 +36,6 @@ import {
 
 import firebase from 'firebase'; // Import Firebase login
 
-array_offerDatas = []
-
-var var_displayName;
-var var_phoneNumber;
-var var_email;
-
 class Offer extends Component {
 
   constructor(){
@@ -93,35 +87,35 @@ class Offer extends Component {
                     </Body>
                         <Right/>
             </Header>
+
             
-            <View style={{
-        flex: 0.5,
-        flexDirection: 'column',
-        justifyContent: 'space-between'
-      }}>
-        <View style={{width: 0, height: 2, backgroundColor: '#FFFFFF'}} />
-                <Text><Title>Title:               {this.props.navigation.state.params.offer}</Title></Text>
-        <View style={{width: 100, height: 20, backgroundColor: '#FFFFFF'}} />
+      
+        <Text><Title>Title: {this.props.navigation.state.params.offer}</Title></Text>
+        <View style={{width: 10000, height: 1, backgroundColor: 'grey'}} />
                   <Text>Description: {this.props.navigation.state.params.description}</Text>
-        <View style={{width: 100, height: 10, backgroundColor: '#FFFFFF'}} />
-                  <Text>Date: {this.props.navigation.state.params.date}</Text>
-        <View style={{width: 100, height: 10, backgroundColor: '#FFFFFF'}} />
-                  <Text><Title>                                                             Price: {this.props.navigation.state.params.price}.- CHF</Title></Text>
-                  <Text>Phone number: {this.state.email}</Text>
-        <View style={{width: 100, height: 60, backgroundColor: '#FFFFFF'}} />        
+        <View style={{width: 10000, height: 1, backgroundColor: 'grey'}} />
+                  <Text color='grey'>Date: {this.props.navigation.state.params.date}</Text>
+        <View style={{width: 10000, height: 1, backgroundColor: 'grey'}} />
+                  <Text><Title>Price: {this.props.navigation.state.params.price}.- CHF</Title></Text>
+        <View style={{width: 10000, height: 1, backgroundColor: 'grey'}} />     
+
           
       
          
-      </View>
                   
-                  
-                  
-           
-           
-    
-    
-    
           </Container>
+                  
+                  
+           
+           
+    
+    
+    
+       
+
+
+
+
         );
       }
     }
